502e70a1b2fae6e12aaa57178a81b97374eaf320
Jett Yuen <jettyuen03@gmail.com>
2022-01-02 20:28:50 -0600

    Finished day 8

--- day08/main.cpp
+++ day08/main.cpp
@@ -1,19 +1,10 @@
 #include <algorithm>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <sstream>
 #include <vector>
-
-// Sort input nums by length, storing the output nums for later decoding.
-// Each line represents input to find code, and an output to decode.
-
-// 1. Find 1(c/f),
-// 2. 7(*(a), c/f),
-// 3. 4(a, c/f, b/d
-// 4. 2(a, *(b), c, *(d), *(e), *(f)) -- Contains either b/d, and either c/f
-// 5. 9(a, b, c, d, e, f, *(g)), -- Contains a->f
-// 6. 8 (a, b, c, d, *(e), f, g)
 
 void splitAtDelim(const std::string &str, const std::string &delim,
                   std::pair<std::string, std::string> &pair);
@@ -21,9 +12,10 @@ void getKey(std::vector<std::string> &input, std::map<char, char> &keyMap);
 void getCount(const std::vector<std::string> &output,
               std::map<char, char> &keyMap,
               std::map<std::string, int> &valueMap,
-              std::map<int, int> &countMap);
+              std::map<int, int> &countMap, int &outSum);
 
 int main(int argc, char **argv) {
+  auto start = std::chrono::high_resolution_clock::now();
   if (argc == 1) {
     std::cerr << "Pass puzzle input as program argument!" << std::endl;
     return 0;
@@ -42,47 +34,52 @@ int main(int argc, char **argv) {
   std::string line{};
   std::ifstream ifs{argv[1]};
   std::pair<std::string, std::string> ioPair = std::make_pair("", "");
+  int outSum{0};
   while (std::getline(ifs, line)) {
     splitAtDelim(line, " | ", ioPair);
-    std::cout << ioPair.first << " | " << ioPair.second << std::endl;
     std::istringstream issI{ioPair.first};
-    std::string wordI{};
-    std::vector<std::string> ssI{};
+    std::string word{};
+    std::vector<std::string> ss{};
     while (issI) {
       char c = issI.get();
       if (c == ' ') {
-        ssI.push_back(wordI);
-        wordI.clear();
+        ss.push_back(word);
+        word.clear();
         continue;
       }
-      wordI.push_back(c);
+      word.push_back(c);
     }
-    getKey(ssI, keyMap);
-    std::vector<std::string> ssO{};
-    std::string wordO{};
+    getKey(ss, keyMap);
+    ss.clear();
     ioPair.second.push_back(' ');
+    word.clear();
     std::istringstream issO{ioPair.second};
     while (issO) {
       char c = issO.get();
       if (c == ' ') {
-        ssO.push_back(wordO);
-        wordO.clear();
+        ss.push_back(word);
+        word.clear();
         continue;
       }
-      wordO.push_back(c);
+      word.push_back(c);
     }
-    getCount(ssO, keyMap, valueMap, countMap);
+    getCount(ss, keyMap, valueMap, countMap, outSum);
   }
 
   int uniques{};
   for (auto &k: countMap) {
     auto first = k.first;
-    std::cout << first << ": " << k.second << std::endl;
     if (first == 1 || first == 4 || first == 7 || first == 8) {
       uniques += k.second;
     }
   }
+
   std::cout << "Uniques: " << uniques << std::endl;
+  std::cout << "Output Sum: " << outSum << std::endl;
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration = duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << "Program took " << duration.count() << " milliseconds"
+            << std::endl;
   ifs.close();
   return 0;
 }
@@ -103,20 +100,17 @@ void getKey(std::vector<std::string> &input, std::map<char, char> &keyMap) {
   char a{'\0'}, b{'\0'}, c{'\0'}, d{'\0'}, f{'\0'}, g{'\0'}, e{'\0'};
   for (const auto &s: input) {
     if (s.length() == 2) {// 1
-      std::cout << "1(c/f): " << s << std::endl;
       cOrF.first = s[0];
       cOrF.second = s[1];
     } else if (s.length() == 3) {// 7
-      std::cout << "7(a): " << s << std::endl;
-      for (auto ch: s) {
+      for (const auto &ch: s) {
         if (ch != cOrF.first && ch != cOrF.second) {
           a = ch;
           keyMap[a] = 'a';
         }
       }
     } else if (s.length() == 4) {// 4
-      std::cout << "4(b/d): " << s << std::endl;
-      for (auto ch: s) {
+      for (const auto &ch: s) {
         if (ch != cOrF.first && ch != cOrF.second) {
           if (bOrD.first == '\0') {
             bOrD.first = ch;
@@ -126,9 +120,8 @@ void getKey(std::vector<std::string> &input, std::map<char, char> &keyMap) {
         }
       }
     } else if (s.length() == 5) {// 2
-      std::cout << "2(b, c, d, f): " << s << std::endl;
       int cFCount{}, bDCount{};
-      for (auto &ch: s) {
+      for (const auto &ch: s) {
         if (ch == cOrF.first) {
           c = cOrF.first;
           f = cOrF.second;
@@ -148,18 +141,15 @@ void getKey(std::vector<std::string> &input, std::map<char, char> &keyMap) {
         }
       }
       if (cFCount == 1 && bDCount == 1) {
-        std::cout << a << ',' << b << ',' << c << ',' << d << ',' << f
-                  << std::endl;
         keyMap[b] = 'b';
         keyMap[c] = 'c';
         keyMap[d] = 'd';
         keyMap[f] = 'f';
       }
     } else if (s.length() == 6) {// 9
-      std::cout << "9(g): " << s << std::endl;
       int satisfiedCount{};
       char possibleG{};
-      for (auto ch: s) {
+      for (const auto &ch: s) {
         if (ch == a || ch == b || ch == c || ch == d || ch == f) {
           satisfiedCount++;
         } else {
@@ -171,21 +161,12 @@ void getKey(std::vector<std::string> &input, std::map<char, char> &keyMap) {
         keyMap[g] = 'g';
       }
     } else if (s.length() == 7) {// 8
-      std::cout << "8(e): " << s << std::endl;
-      std::cout << keyMap[a] << ", " << a << std::endl;
-      std::cout << keyMap[b] << ", " << b << std::endl;
-      std::cout << keyMap[c] << ", " << c << std::endl;
-      std::cout << keyMap[d] << ", " << d << std::endl;
-      for (auto ch: s) {
+      for (const auto &ch: s) {
         if (ch != a && ch != b && ch != c && ch != d && ch != f && ch != g) {
           e = ch;
           keyMap[e] = 'e';
-          std::cout << keyMap[e] << ", " << e << std::endl;
         }
       }
-      std::cout << keyMap[f] << ", " << f << std::endl;
-      std::cout << keyMap[g] << ", " << g << std::endl;
-      std::cout << std::endl;
     }
   }
 }
@@ -193,18 +174,19 @@ void getKey(std::vector<std::string> &input, std::map<char, char> &keyMap) {
 void getCount(const std::vector<std::string> &output,
               std::map<char, char> &keyMap,
               std::map<std::string, int> &valueMap,
-              std::map<int, int> &countMap) {
+              std::map<int, int> &countMap, int &outSum) {
+  int place{1000};
   for (const std::string &s: output) {
     std::string ts{s};
     for (auto &c: ts) {
       c = keyMap[c];
     }
-    std::cout << s << std::endl;
     std::sort(ts.begin(), ts.end());
-    std::cout << ts << std::endl;
     if (valueMap.count(ts) == 1) {
-      int i = valueMap[ts];
-      countMap[i]++;
+      int n = valueMap[ts];
+      outSum += place * n;
+      countMap[n]++;
     }
+    place /= 10;
   }
 }
